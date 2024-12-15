e39d5d9c2d43300a4c29fbfc29330b9f81cf042e
Yosstin <Yosstincode@gmail.com>
2024-12-15 11:15:06 -0500

    keyboardmanager translated, icons skills fixed

--- src/i18n/types.ts
+++ src/i18n/types.ts
@@ -1,134 +1,165 @@
 export interface Basics {
-    name: string;
-    label: string;
-    image: string;
-    email: string;
-    phone: string;
+  name: string;
+  label: string;
+  image: string;
+  email: string;
+  phone: string;
+  url: string;
+  summary: string;
+  theme: string;
+  location: {
+    address: string;
+    postalCode: string;
+    city: string;
+    countryCode: string;
+    region: string;
+  };
+  profiles: {
+    network: string;
+    username: string;
     url: string;
-    summary: string;
-    theme: string;
-    location: {
-        address: string;
-        postalCode: string;
-        city: string;
-        countryCode: string;
-        region: string;
-    };
-    profiles: {
-        network: string;
-        username: string;
-        url: string;
-    }[];
+  }[];
 }
 
 export interface Certificate {
-    name: string
-    date: string
-    issuer: string
-    url: string
-    relevant: boolean
+  name: string;
+  date: string;
+  issuer: string;
+  url: string;
+  relevant: boolean;
 }
 
-export type Certificates = Certificate[]
+export type Certificates = Certificate[];
 
 export interface CertificatesUI {
-    title: string
-    certificates: Certificates
+  title: string;
+  certificates: Certificates;
 }
 
 export interface Education {
-    institution: string
-    url: string
-    area: string
-    studyType: string
-    startDate: string
-    endDate?: string
-    score: string
-    details: string[]
+  institution: string;
+  url: string;
+  area: string;
+  studyType: string;
+  startDate: string;
+  endDate?: string;
+  score: string;
+  details: string[];
 }
 
-export type Educations = Education[]
+export type Educations = Education[];
 
 export interface EducationUI {
-    title: string
-    educations: Educations
+  title: string;
+  educations: Educations;
 }
 
 export interface Project {
-    name: string
-    isActive: boolean
-    description: string
-    highlights: string[]
-    url: string
-    github?: string
+  name: string;
+  isActive: boolean;
+  description: string;
+  highlights: string[];
+  url: string;
+  github?: string;
 }
 
-export type Projects = Project[]
+export type Projects = Project[];
 
 export interface ProjectsUI {
-    title: string
-    projects: Projects
+  title: string;
+  projects: Projects;
 }
 
 export interface Skill {
-    name: string
-    level: string
-    keywords: string[]
+  name: string;
+  level: string;
+  keywords: string[];
 }
 
-export type Skills = Skill[]
+export type Skills = Skill[];
 
 export interface SkillsUI {
-    title: string
-    skills: Skills
+  title: string;
+  skills: Skills;
 }
 
 export interface Work {
-    name: string
-    position: string
-    url: string
-    location_type: string
-    location: string
-    startDate: string
-    endDate: string
-    summary: string
-    achievements: string[] | null
-    responsibilities: string[]
-    skills: string[]
+  name: string;
+  position: string;
+  url: string;
+  location_type: string;
+  location: string;
+  startDate: string;
+  endDate: string;
+  summary: string;
+  achievements: string[] | null;
+  responsibilities: string[];
+  skills: string[];
 }
 
-export type Works = Work[]
+export type Works = Work[];
 
 export interface WorkUI {
-    title: string
-    works: Works
+  title: string;
+  works: Works;
 }
 
 export interface Experience {
-    title: string
-    summary: string
-    responsabilities: string
-    achievements: string
+  title: string;
+  summary: string;
+  responsabilities: string;
+  achievements: string;
 }
 
 export interface Hero {
-    'email.copy': string
+  "email.copy": string;
 }
 
 export interface Months {
-    "0": string
-    "1": string
-    "2": string
-    "3": string
-    "4": string
-    "5": string
-    "6": string
-    "7": string
-    "8": string
-    "9": string
-    "10": string
-    "11": string
+  "0": string;
+  "1": string;
+  "2": string;
+  "3": string;
+  "4": string;
+  "5": string;
+  "6": string;
+  "7": string;
+  "8": string;
+  "9": string;
+  "10": string;
+  "11": string;
 }
 
+// keyBoardManager: {
+//     socialSection: { name: "Social networks" },
+//     visit: "Visit",
+//     press: "Press",
+//     callActionPress: "to open the command palette",
+//     placeholder: "Search command...",
+//     printSection: {
+//       name: "Actions",
+//       title: "Print CV",
+//     },
+//   },
 
-export type LanguageList = 'en' | 'es'
+export interface KeyBoardManagerUI {
+  socialSection: { name: string };
+  visit: string;
+  press: string;
+  callActionPress: string;
+  placeholder: string;
+  actionsSection: {
+    name: string;
+    actions: {
+      name: string;
+      icon: string;
+    }[];
+  };
+  footerSection: {
+    toSelect: string;
+    toNavigate: string;
+    toClose: string;
+  };
+}
+
+export type LanguageList = "en" | "es";
--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,120 +1,156 @@
 //*******************Castellano*************************/
-import basics from '@/i18n/es/basics.json';
-import Certificates from '@/i18n/es/certificates.json';
-import education from '@/i18n/es/education.json';
-import projects from '@/i18n/es/projects.json';
-import skills from '@/i18n/es/skills.json';
-import work from '@/i18n/es/work.json';
-import months from '@/i18n/es/months.json';
+import basics from "@/i18n/es/basics.json";
+import Certificates from "@/i18n/es/certificates.json";
+import education from "@/i18n/es/education.json";
+import projects from "@/i18n/es/projects.json";
+import work from "@/i18n/es/work.json";
+import months from "@/i18n/es/months.json";
 //*******************Inglés*************************/
-import basicsEn from '@/i18n/en/basics.json';
-import CertificatesEn from '@/i18n/en/certificates.json';
-import educationEn from '@/i18n/en/education.json';
-import projectsEn from '@/i18n/en/projects.json';
-import skillsEn from '@/i18n/en/skills.json';
-import workEn from '@/i18n/en/work.json';
-import monthsEn from '@/i18n/en/months.json';
+import basicsEn from "@/i18n/en/basics.json";
+import CertificatesEn from "@/i18n/en/certificates.json";
+import educationEn from "@/i18n/en/education.json";
+import projectsEn from "@/i18n/en/projects.json";
+import workEn from "@/i18n/en/work.json";
+import monthsEn from "@/i18n/en/months.json";
 //*************************************************/
-import languages from '@/i18n/languages.json';
+import languages from "@/i18n/languages.json";
 
 export const ui = {
-    es: {
-        basics,
-        certificates: {
-          title: 'Certificados',
-          certificates: [...Certificates],
-        },
-        education: {
-          title: 'Educación',
-          educations: [...education],
-        },
-        projects: {
-          title: 'Proyectos',
-          projects: [...projects],
-        },
-        skills:{
-          title: 'Habilidades',
-          skills: [...skills],
-        },
-        work: {
-            title: 'Experiencia laboral',
-            works: [...work],
-        },
-        present: {
-            label: 'Actualmente'
-        },
-        showMore: {
-            label: 'Ver más'
-        },
-        showLess: {
-            label: 'Ver menos'
-        },
-        portfolio: {
-            label: 'Portafolio'
-        },
-        experience: {
-          title: 'Experiencia',
-          summary: 'Resumen',
-          responsabilities: 'Responsabilidades',
-          achievements: 'Logros',
-        },
-        about:{
-          title: 'Sobre mí'
-        },
-        hero: {
-          'email.copy': 'Copiado!',
-        },
-        months
+  es: {
+    basics,
+    certificates: {
+      title: "Certificados",
+      certificates: [...Certificates],
     },
-    en: {
-        basics: basicsEn,
-        certificates: {
-          title: 'Certificates',
-          certificates: [...CertificatesEn],
-        },
-        education: {
-          title: 'Education',
-          educations: [...educationEn],
-        },
-        projects: {
-          title: 'Projects',
-          projects: [...projectsEn],
-        },
-        skills:{
-          title: 'Skills',
-          skills: [...skillsEn],
-        },
-        work: {
-            title: 'Work experience',
-            works: [...workEn],
-        },
-        present: {
-            label: 'Present'
-        },
-        showMore: {
-            label: 'Show more'
-        },
-        showLess: {
-          label: 'Show less'
-        },
-        portfolio: {
-            label: 'Portfolio'
-        },
-        experience: {
-          title: 'Experience',
-          summary: 'Summary',
-          responsabilities: 'Responsabilities',
-          achievements: 'Achievements',
-        },
-        about:{
-          title: 'About me'
-        },
-        hero: {
-          'email.copy': 'Email copied!',
-        },
-        months: monthsEn
-    }
-}
+    education: {
+      title: "Educación",
+      educations: [...education],
+    },
+    projects: {
+      title: "Proyectos",
+      projects: [...projects],
+    },
+    skills: {
+      title: "Habilidades",
+    },
+    work: {
+      title: "Experiencia laboral",
+      works: [...work],
+    },
+    present: {
+      label: "Actualmente",
+    },
+    showMore: {
+      label: "Ver más",
+    },
+    showLess: {
+      label: "Ver menos",
+    },
+    portfolio: {
+      label: "Portafolio",
+    },
+    experience: {
+      title: "Experiencia",
+      summary: "Resumen",
+      responsabilities: "Responsabilidades",
+      achievements: "Logros",
+    },
+    about: {
+      title: "Sobre mí",
+    },
+    hero: {
+      "email.copy": "Copiado!",
+    },
+    months,
+    keyBoardManager: {
+      socialSection: { name: "Redes sociales" },
+      visit: "Visitar",
+      press: "Presionar",
+      callActionPress: "para abrir la paleta de comandos",
+      placeholder: "Buscar comando...",
+      actionsSection: {
+        name: "Acciones",
+        actions: [
+          { name: "Imprimir CV", icon: "print" },
+          { name: "Cambiar idioma", icon: "language" },
+          { name: "Cambiar tema", icon: "theme" },
+        ],
+      },
+      footerSection: {
+        toSelect: "Para seleccionar",
+        toNavigate: "Para navegar",
+        toClose: "Para cerrar",
+      },
+    },
+  },
+  en: {
+    basics: basicsEn,
+    certificates: {
+      title: "Certificates",
+      certificates: [...CertificatesEn],
+    },
+    education: {
+      title: "Education",
+      educations: [...educationEn],
+    },
+    projects: {
+      title: "Projects",
+      projects: [...projectsEn],
+    },
+    skills: {
+      title: "Skills",
+    },
+    work: {
+      title: "Work experience",
+      works: [...workEn],
+    },
+    present: {
+      label: "Present",
+    },
+    showMore: {
+      label: "Show more",
+    },
+    showLess: {
+      label: "Show less",
+    },
+    portfolio: {
+      label: "Portfolio",
+    },
+    experience: {
+      title: "Experience",
+      summary: "Summary",
+      responsabilities: "Responsabilities",
+      achievements: "Achievements",
+    },
+    about: {
+      title: "About me",
+    },
+    hero: {
+      "email.copy": "Email copied!",
+    },
+    months: monthsEn,
+    keyBoardManager: {
+      socialSection: { name: "Social networks" },
+      visit: "Visit",
+      press: "Press",
+      callActionPress: "to open the command palette",
+      placeholder: "Search command...",
+      actionsSection: {
+        name: "Actions",
+        actions: [
+          { name: "Print CV", icon: "print" },
+          { name: "Change language", icon: "language" },
+          { name: "Change theme", icon: "theme" },
+        ],
+      },
+      footerSection: {
+        toSelect: "To select",
+        toNavigate: "To navigate",
+        toClose: "To close",
+      },
+    },
+  },
+};
 
 interface LanguageList {
   [key: string]: {
@@ -126,4 +162,4 @@ interface LanguageList {
 
 export const languageList: LanguageList = languages;
 
-export const defaultLang = 'es';
+export const defaultLang = "es";
